185561f7664ef453ba4d3bbd3d313b6c1867ac69
jet2tlf <jet2tlf@gmail.com>
2024-05-29 23:10:36 -0300

    pass 3st stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,13 +8,15 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	stdin := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	in, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Println(err.Error())
+		in, err := stdin.ReadString('\n')
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		fmt.Printf("%s: command not found\n", strings.TrimRight(in, "\n"))
 	}
-
-	fmt.Printf("%s: command not found\n", strings.TrimRight(in, "\n"))
 }
